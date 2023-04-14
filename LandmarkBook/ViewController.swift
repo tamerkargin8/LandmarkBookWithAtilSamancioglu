39542ed35b75c0a955804296eb4afe99e3e43ffd
Tamer KARGIN <96518304+tamerkargin8@users.noreply.github.com>
2023-04-14 03:37:14 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Tamer KARGIN on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
